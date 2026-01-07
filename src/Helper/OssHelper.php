2cfa9276ca261c6f22601a8291205ade218b4bb0
BennKwok <benn_kwok@foxmail.com>
2026-01-07 16:15:28 +0800

    新增：用户信息帮助方法

---
+++ src/Helper/OssHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+use Digood\Sso\Services\SsoService;
+
+if (!function_exists('oss_user_info')) {
+    /**
+     * 用户信息
+     * @return array|bool
+     */
+    function oss_user_info(): array|bool
+    {
+        return (new SsoService())->getUserInfo();
+    }
+}
+
+if (!function_exists('oss_user_id')) {
+    /**
+     * 用户标识
+     * @return string|false
+     */
+    function oss_user_id(): string|false
+    {
+        return (new SsoService())->getUserId();
+    }
+}
+
+if (!function_exists('oss_user_roles')) {
+    /**
+     * 用户角色清单
+     * @return array
+     */
+    function oss_user_roles(): array
+    {
+        return (new SsoService())->getUserRoles();
+    }
+}
+
+if (!function_exists('oss_user_is_digood')) {
+    /**
+     * 用户是否为多谷内部员工
+     * @return bool
+     */
+    function oss_user_is_digood(): bool
+    {
+        return (new SsoService())->isRoleDigood();
+    }
+}
