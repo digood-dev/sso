144ef645d4bdb3f913ff7e4543dac4a530d5ff6c
BennKwok <benn_kwok@foxmail.com>
2026-01-08 15:57:18 +0800

    优化：权限提示文案

--- src/Http/Controllers/GoController.php
+++ src/Http/Controllers/GoController.php
@@ -14,10 +14,10 @@ class GoController
      */
     function sub_system(Request $request)
     {
-        if ($request->session()->get('sso_login', false)) response('<h1>无法流转至其它子系统<h1><hr>当前登录态来自其它系统流转，不支持继续流转至其它子系统', 500);
+        if ($request->session()->get('sso_login', false)) response('<h1>无法流转至其它子系统<h1><hr><small>当前登录态来自其它系统流转，不支持继续流转至其它子系统</small>', 500);
 
         $token = (new SsoService())->client()->getAccessToken();
-        if (empty($token)) return response('<h1>无法提取的你联合账户TOKEN<h1><hr><p>可能的原因：</p><p>可能未登录SSO多谷联合账户</p><p>从其它系统流转的登录态</p>', 500);
+        if (empty($token)) return response('<h1>无法提取的你联合账户TOKEN<h1><hr><small>可能的原因：</small><small>未使用SSO多谷联合账户进行登录</small><small>从其它系统流转的登录态</small>', 500);
 
         $redirect_to = base64_decode($request->input('redirect_to'));
         $host = parse_url($redirect_to, PHP_URL_HOST);
